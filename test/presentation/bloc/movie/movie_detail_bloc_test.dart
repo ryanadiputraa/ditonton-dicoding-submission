34daa9e5307ce3f478cacd58a9dbe1ebe63a2312
Ryan Adi Putra <ryannadiputraa@gmail.com>
2022-05-02 16:27:11 +0800

    Restructured bloc test

--- test/presentation/bloc/movie/movie_detail_bloc_test.dart
+++ test/presentation/bloc/movie/movie_detail_bloc_test.dart
@@ -7,7 +7,7 @@ import 'package:flutter_test/flutter_test.dart';
 import 'package:mockito/annotations.dart';
 import 'package:mockito/mockito.dart';
 
-import '../../dummy_data/dummy_objects.dart';
+import '../../../dummy_data/dummy_objects.dart';
 import 'movie_detail_bloc_test.mocks.dart';
 
 @GenerateMocks([GetMovieDetail])
--- test/presentation/bloc/movie/search_bloc_test.dart
+++ test/presentation/bloc/movie/search_bloc_test.dart
@@ -6,7 +6,7 @@ import 'package:ditonton/presentation/bloc/movie/search/search_bloc.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:mockito/annotations.dart';
 import 'package:mockito/mockito.dart';
-import '../../dummy_data/dummy_objects.dart';
+import '../../../dummy_data/dummy_objects.dart';
 import 'search_bloc_test.mocks.dart';
 
 @GenerateMocks([SearchMovies])
